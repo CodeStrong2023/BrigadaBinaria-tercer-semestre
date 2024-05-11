08c8f5745e3573f94afea6304481ac80254631cd
Sebastian Betancurth <33584221+Ssbas@users.noreply.github.com>
2024-05-10 23:35:03 -0300

    clase 2

---
+++ Python/Semana1-2/Clase 2/TetsConstantes.py
@@ -0,0 +1,9 @@
+from constantes import *
+from constantes import Matematica as Mate
+
+print(VConstante)
+#VConstante = "Nuevo valor para la variable constante"
+#print(VConstante)
+
+print(Matematica)
+print(Mate)
