1c94085b1afc769807d43a09bb2a357c895a4b00
Santiago Castro <136345439+Santiago-Castro8@users.noreply.github.com>
2024-05-17 22:12:42 -0300

    Delete Python directory

--- Python/Semana1-2/Clase 2/TetsConstantes.py
+++
@@ -1,9 +0,0 @@
-from constantes import *
-from constantes import Matematica as Mate
-
-print(VConstante)
-#VConstante = "Nuevo valor para la variable constante"
-#print(VConstante)
-
-print(Matematica)
-print(Mate)
--- Python/Semana1-2/Clase 2/constantes.py
+++
@@ -1,3 +0,0 @@
-class Matematica:
-    PI = 3.1416
-VConstante = "Esta es una variable constante"
